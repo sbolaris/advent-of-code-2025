4eeaccd83aeddac1cf9e179d06bc8c0e78d57110
sbolaris <stephen.bolaris@bio-rad.com>
2025-12-03 15:01:10 -0800

    update jolt metere and added test input from the website

--- day3/joltage.go
+++ day3/joltage.go
@@ -2,12 +2,85 @@
 package main
 
 import "fmt"
+import "strconv"
+import "os"
+import "bufio"
 
 // function for part 1
+// each line of input is the single bank of batteries
+// turn on exactly two bateries that two are the max jotlage
+// this is the two max digits in the list
+// return the two numbeers as a 2 digit int
+func jotlage_meter(batt_bank string) int {
+	max_joltage := 0
+	// parse input into list of integers
+	for i, char := range batt_bank {
+		digit, err := strconv.Atoi(string(char))
+		if err != nil {
+			fmt.Println("Error converting character to integer:", err)
+			continue
+		}
+		for j, char2 := range batt_bank[i+1:] {
+			if i == j {
+				continue // skip same index
+			}
+			digit2, err := strconv.Atoi(string(char2))
+			if err != nil {
+				fmt.Println("Error converting character to integer:", err)
+				continue
+			}
+			combined_joltage := strconv.Itoa(digit) + strconv.Itoa(digit2)
+			combined_int, err := strconv.Atoi(combined_joltage)
+			if err != nil {
+				fmt.Println("Error converting combined joltage to integer:", err)
+				continue
+			}
+			if combined_int > max_joltage {
+				max_joltage = combined_int
+				//fmt.Println("Max digits are:", digit, digit2)
+			}
+		}
+		
+	}
+	// return their final number combined as int
+	return max_joltage
+}
+func sumIntSlice(ints []int) int {
+	sum := 0
+	for _, v := range ints {
+		sum += v
+	}
+	return sum
+}
+
 
 // function for part 2
 
 // main function
 func main() {
 	fmt.Println("Advent of Code 2025 - Day 3")
+	//read input file to pass to functions 
+	file_path := "./test_input.txt"
+	file, err := os.Open(file_path)
+	if err != nil {
+		fmt.Println("Error reading file:", err)
+		return
+	}
+	total_joltage := []int{}
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines := scanner.Text()
+		// call joltage meter function and print result
+		fmt.Println("Processing line: ", string(lines))
+		joltage := jotlage_meter(string(lines))
+		fmt.Println("Joltage Meter Reading: ", joltage)
+		total_joltage = append(total_joltage, joltage)
+	}
+	sum_joltage := sumIntSlice(total_joltage)
+	fmt.Println("Total Joltage Meter Reading: ", sum_joltage)
+	
+	
+	// call part 1 function and print result
+
+	// call part 2 function and print result
 }
