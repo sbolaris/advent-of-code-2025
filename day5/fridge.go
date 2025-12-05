86dd18d05e77497aac5532c9bd0b65895176f37c
sbolaris <stephen.bolaris@bio-rad.com>
2025-12-05 15:31:32 -0800

    trying out going by sorting first number and then merging to speed it up

--- day5/fridge.go
+++ day5/fridge.go
@@ -8,6 +8,7 @@ import(
 	"bufio"
 	"strings"
 	"sync"
+	"sort"
 )
 
 //part A 
@@ -33,19 +34,53 @@ func countFreshItems(fresh_ranges []string, item_ids []int) int {
 // using only the ranges provided in the fresh range determine how many items are fresh
 // the ranges can overlap so need to account for that 16-20 and 18-22 only count 16-22 once
 // need to sort and merge the ranges first then count total items based on merged ranges
+// 3 - 5
+// 10 - 14
+// 12 - 18
+// 16 - 20
 func countAllPossibleFreshItems(fresh_ranges []string) int {
-	total_possible_fresh := 0
-	covered_ids := make(map[int]bool)
-	var mu sync.Mutex
-	range_chan := make(chan string, len(fresh_ranges))
-	//start workers
-	for i := 0; i < 6; i++ {
-		go countPossibleItems(mu, range_chan, covered_ids)
+	//sort by first number in range
+	sort.Slice(fresh_ranges, func(i, j int) bool {
+		i_start, _ := strconv.Atoi(strings.Split(fresh_ranges[i], "-")[0])
+		j_start, _ := strconv.Atoi(strings.Split(fresh_ranges[j], "-")[0])
+		return i_start < j_start
+	})
+	//merge overlapping ranges
+	merged_ranges := []string{}
+	current_range := fresh_ranges[0]
+	for i := 1; i < len(fresh_ranges); i++ {
+		current_start_end := strings.Split(current_range, "-")
+		current_start, _ := strconv.Atoi(current_start_end[0])
+		current_end, _ := strconv.Atoi(current_start_end[1])
+		next_start_end := strings.Split(fresh_ranges[i], "-")
+		next_start, _ := strconv.Atoi(next_start_end[0])
+		next_end, _ := strconv.Atoi(next_start_end[1])
+		if next_start <= current_end { //overlap
+			if next_end > current_end {
+				current_end = next_end
+			}
+			current_range = fmt.Sprintf("%d-%d", current_start, current_end)
+		} else {
+			merged_ranges = append(merged_ranges, current_range)
+			current_range = fresh_ranges[i]
+		}
 	}
-	return total_possible_fresh
+	merged_ranges = append(merged_ranges, current_range) //add last range
+
+	//count total items in merged ranges
+	total_fresh := 0
+	for _, merged_range := range merged_ranges {
+		range_parts := strings.Split(merged_range, "-")
+		start, _ := strconv.Atoi(range_parts[0])
+		end, _ := strconv.Atoi(range_parts[1])
+		total_fresh += (end - start + 1)
+	}	
+	return total_fresh
 }
 
-//subroutines
+////////////////////
+//  subroutines  //
+//////////////////
 func isFresh(item_id int, fresh_range string, is_fresh chan<- bool) {
 	//start and end of range
 	ranges := strings.Split(fresh_range, "-")
@@ -65,27 +100,6 @@ func isFresh(item_id int, fresh_range string, is_fresh chan<- bool) {
 		is_fresh <- true
 	} else {
 		is_fresh <- false
-	}
-}
-
-func countPossibleItems(mu sync.Mutex, range_chan chan<- string, covered_ids map[int]bool) {
-	for fresh_range := range range_chan {
-		ranges := strings.Split(fresh_range, "-")
-		if len(ranges) != 2 {
-			fmt.Println("Invalid range format:", fresh_range)
-			continue
-		}
-		start, err1 := strconv.Atoi(ranges[0])
-		end, err2 := strconv.Atoi(ranges[1])
-		if err1 != nil || err2 != nil {
-			fmt.Println("Error converting range bounds to integers:", err1, err2)
-			continue
-		}
-		mu.Lock()
-		for id := start; id <= end; id++ {
-			covered_ids[id] = true
-		}
-		mu.Unlock()
 	}
 }
 
@@ -131,7 +145,7 @@ func access_database(file_path string) ([]string, []int) {
 func main() {
 	var wg sync.WaitGroup
 	fmt.Println("Advent of Code 2025 Day 5 inventory management")
-	file_path := "./input.txt"
+	file_path := "./test_input.txt"
 	fresh_range, item_ids := access_database(file_path)
 	fmt.Println("Fresh Range: ", fresh_range)
 	fmt.Println("Item IDs: ", item_ids)
