b15c0a6e1d588f2585c3b2413bdfd201a8d00e9c
sbolaris <stephen.bolaris@bio-rad.com>
2025-12-06 20:36:43 -0800

    fixed vertical math for broken conditions and out of bounds issue.

--- day6/cephlamath.go
+++ day6/cephlamath.go
@@ -64,65 +64,84 @@ func cephlamath_solver(worksheet [][]string) int {
 func vertical_math (worksheet [][]rune) int{
 	// go through runes the check that that the last one by be a math operator
 	final_result := 0
-	// iterate over columns
+	// iterate over columns from right to left
 	num_columns := len(worksheet[0])
 	num_rows := len(worksheet)
-	// Can you do this recusrively?
+	
 	vertical_nums := []int{}
-	for col := num_columns; 0 < num_columns; col-- {
+	
+	for col := num_columns - 1; col >= 0; col-- {
 		// get the operand from the last row
 		operand := string(worksheet[num_rows-1][col])
-		// perform operation on all numbers above it
-		column_result := 0
-		current_number, err := strconv.Atoi(string(worksheet[0:num_rows-1][col]))
-		if err != nil {
-			fmt.Println("Error converting to integer:", err)
-			continue
+		
+		// Build the number from all rows above the operand row for this column
+		number_str := ""
+		for row := 0; row < num_rows-1; row++ {
+			char := worksheet[row][col]
+			// Skip spaces when building the number
+			if char != ' ' {
+				number_str += string(char)
+			}
 		}
-		fmt.Println("current number is: ", current_number)
-		vertical_nums = append(vertical_nums, current_number) 
-		if operand != ""{
-			for _, num := range(vertical_nums){
+		
+		// Convert the built string to integer if it's not empty
+		if number_str != "" {
+			current_number, err := strconv.Atoi(number_str)
+			if err != nil {
+				fmt.Println("Error converting to integer:", err, "for string:", number_str)
+				continue
+			}
+			fmt.Println("Current number is:", current_number, "from column", col)
+			vertical_nums = append(vertical_nums, current_number)
+		}
+		
+		// If we have an operand and numbers to operate on
+		if operand != " " && operand != "" && len(vertical_nums) > 0 {
+			column_result := 0
+			for i, num := range vertical_nums {
 				switch operand {
 				case "+":
 					column_result += num
 				case "-":
-					column_result -= num
+					if i == 0 {
+						column_result = num
+					} else {
+						column_result -= num
+					}
 				case "*":
-					//if row == 0 {
-						//column_result = num
-					//} else {
+					if i == 0 {
+						column_result = num
+					} else {
 						column_result *= num
-					//}
+					}
 				case "/":
-					// if row == 0 {
-					// 	column_result = num
-					// } else {
+					if i == 0 {
+						column_result = num
+					} else {
 						if num != 0 {
 							column_result /= num
 						} else {
 							fmt.Println("Division by zero encountered.")
 						}
-					//}
+					}
 				default:
 					fmt.Println("Unknown operand:", operand)
 				}
 			}
+			fmt.Println("Column result for operand", operand, ":", column_result)
 			final_result += column_result
-			vertical_nums = []int{}
+			vertical_nums = []int{} // Reset for next operation
 		}
 	}
 	return final_result
-
 }
-
 
 //main function
 func main() {
 	fmt.Println("Cephlamath Day 6")
 	// open input file
 	//file, err := os.Open("test_input.txt")
-	file, err := os.Open("input.txt")
+	file, err := os.Open("test_input.txt")
 	if err != nil {
 		fmt.Println("Error opening file:", err)
 		return
@@ -135,22 +154,28 @@ func main() {
 		// process line
 		math_problems = append(math_problems, feilds)
 	}
-	//file.Close()
+	// file.Close()
 	// call cephlamath solver function
 	result := cephlamath_solver(math_problems)
 	fmt.Println("Cephlamath Final Result: ", result)
 	// part b requires the file be read in differently and not split into fields
-	scanner = bufio.NewScanner(file)
+	_, err = file.Seek(0, 0) // reset file pointer to beginning
+	if err != nil {
+		fmt.Println("Error resetting file pointer:", err)
+		return
+	}
+	scanner2 := bufio.NewScanner(file)
 	problems_math := [][]rune{}
-	for scanner.Scan() {
-		lines := scanner.Text()
-		lineOfRunes := []rune(lines)
-		problems_math = append(problems_math, lineOfRunes)
+	for scanner2.Scan() {
+		line := scanner2.Text()
+		runes := []rune(line)
+		// process line
+		problems_math = append(problems_math, runes)
 	}
 	file.Close()
 	//call part B where it read the numbers as the cols instead of human math
+	fmt.Println("Starting Vertical Math Solver")
+	fmt.Println("Problems Math: ", problems_math)
 	resultB := vertical_math(problems_math)
 	fmt.Println("Cephlamath Final Result: ", resultB)
-
-
 }
