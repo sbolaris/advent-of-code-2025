f48e0625632972b17fc75b9d6c1e9ecca4b53414
sbolaris <stephen.bolaris@bio-rad.com>
2025-12-07 22:49:45 -0800

    working for part A

--- day7/teleporter.go
+++ day7/teleporter.go
@@ -7,6 +7,8 @@ import(
 	"bufio"
 	"strings"
 	"time"
+	"slices"
+
 )
 
 // function for part A
@@ -24,15 +26,23 @@ func tachyon_spliter(teleporter_data [][]rune) int {
 	// iterate over each row
 	for row := 1; row < num_rows; row++ {
 		tachyon_beam, splits = tachyon_emitter(teleporter_data[row], tachyon_beam)
+		//debug statement
+		//fmt.Println("Number of splits: ", splits, "in row ", row)
 		final_splits += splits
 	}
 	return final_splits
 }
 
 //function for part B
+func quantam_entanglement(teleporter_data [][]rune) int {
+	time_lines := 0
 
+	return time_lines
+}
 
 //subroutines if needed
+// have to check that you dont add duplicate positions to the beam positions
+// this causes the beam to be counted wrong and could lead exploded counts
 func tachyon_emitter(row []rune, beam_positions []int) ([]int, int) {
 	new_beam_positions := []int{}
 	splits := 0
@@ -41,36 +51,35 @@ func tachyon_emitter(row []rune, beam_positions []int) ([]int, int) {
 		if pos >= 0 && pos < len(row) {
 			if row[pos] == '^' {
 				// split the beam
-				new_beam_positions = append(new_beam_positions, pos-1)
-				new_beam_positions = append(new_beam_positions, pos+1)
+				//check if new possitions already exist in new_beam_positions
+				if slices.Contains(new_beam_positions, pos-1) == false {
+					new_beam_positions = append(new_beam_positions, pos-1)
+				}
+				if slices.Contains(new_beam_positions, pos+1) == false {
+					new_beam_positions = append(new_beam_positions, pos+1)
+				}
 				splits++
 			} else if row[pos] == '.' {
 				// continue straight down
-				new_beam_positions = append(new_beam_positions, pos)
+				if slices.Contains(new_beam_positions, pos) == false {
+					new_beam_positions = append(new_beam_positions, pos)
+				}
 			}
 		}
 	}
-
+	//debug statement
+	//fmt.Println("Beam positions after row:", new_beam_positions)
 	return new_beam_positions, splits
 }
 
 // main function to read input solve issues with tachyonic teleporter
 func main() {
 	fmt.Println("Tachyonic Teleporter Calibration Program")
-	
-	// Check if benchmark mode is requested
-	if len(os.Args) > 1 && os.Args[1] == "benchmark" {
-		RunCustomBenchmarks()
-		return
+	fileName := "test_input.txt"
+	if len(os.Args) > 1{
+		fileName = os.Args[1]
+		// read input file
 	}
-	
-	// Try test input first, then input.txt
-	fileName := "input.txt"
-	if _, err := os.Stat("test_input.txt"); err == nil {
-		fileName = "test_input.txt"
-		fmt.Println("Using test_input.txt for debugging")
-	}
-	
 	// read input file
 	file, err := os.Open(fileName)
 	if err != nil {
@@ -92,4 +101,6 @@ func main() {
 	duration := time.Since(start)
 	fmt.Printf("Tachyonic Teleporter Calibration Result: %d\n", result)
 	fmt.Printf("Time taken for tachyon_spliter: %v\n", duration)
+
+	//
 }
