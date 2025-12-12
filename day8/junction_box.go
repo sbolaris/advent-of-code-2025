8079abc481c65b80586e0421f502ceda64e83218
sbolaris <stephen.bolaris@bio-rad.com>
2025-12-12 09:02:09 -0800

    part a completed adding part b

--- day8/junction_box.go
+++ day8/junction_box.go
@@ -16,10 +16,26 @@ import(
 // given x,y,z coordinates, find groups of connected junction boxes
 // by repeatedly connecting the closest unconnected pairs
 func circuits_optimizer(junction_data [][]int, total_con int) int {
-	// Create a structure to hold all distances
-	connections_count := 0
+	// Create list of ALL possible connections with distances
+	type connection struct {
+		j1, j2 int
+		dist   float64
+	}
 	
-	// Use Union-Find for simpler connection tracking
+	var all_connections []connection
+	for i := 0; i < len(junction_data); i++ {
+		for j := i + 1; j < len(junction_data); j++ {
+			dist := distance(junction_data[i], junction_data[j])
+			all_connections = append(all_connections, connection{i, j, dist})
+		}
+	}
+	
+	// KEY INSIGHT: Sort ALL distances globally (shortest first)
+	sort.Slice(all_connections, func(i, j int) bool {
+		return all_connections[i].dist < all_connections[j].dist
+	})
+	
+	// Union-Find setup
 	parent := make([]int, len(junction_data))
 	groupSize := make([]int, len(junction_data))
 	for i := range parent {
@@ -27,7 +43,6 @@ func circuits_optimizer(junction_data [][]int, total_con int) int {
 		groupSize[i] = 1
 	}
 	
-	// Find root of a group
 	var find func(int) int
 	find = func(x int) int {
 		if parent[x] != x {
@@ -36,65 +51,60 @@ func circuits_optimizer(junction_data [][]int, total_con int) int {
 		return parent[x]
 	}
 	
-	// Union two groups
-	union := func(x, y int) {
+	union := func(x, y int) bool {
 		rootX, rootY := find(x), find(y)
-		if rootX != rootY {
-			if groupSize[rootX] < groupSize[rootY] {
-				rootX, rootY = rootY, rootX
-			}
-			parent[rootY] = rootX
-			groupSize[rootX] += groupSize[rootY]
-			connections_count++
+		if rootX == rootY {
+			return false
 		}
-	}
-	
-	// Create list of all possible connections with distances
-	type edge struct {
-		i, j int
-		dist int
-	}
-	
-	edges := []edge{}
-	for i := 0; i < len(junction_data); i++ {
-		for j := i + 1; j < len(junction_data); j++ {
-			dist := distance(junction_data[i], junction_data[j])
-			edges = append(edges, edge{i, j, int(dist * 1000)})
+		if groupSize[rootX] < groupSize[rootY] {
+			rootX, rootY = rootY, rootX
 		}
+		parent[rootY] = rootX
+		groupSize[rootX] += groupSize[rootY]
+		return true
 	}
 	
-	// Sort edges by distance (shortest first)
-	sort.Slice(edges, func(i, j int) bool {
-		return edges[i].dist < edges[j].dist
-	})
-	
-	// Connect closest pairs until we reach total_con connections
-	for _, e := range edges {
-		if connections_count >= total_con {
+	// Connect the shortest total_con connections globally
+	connections_made := 0
+	actual_merges := 0
+	for _, conn := range all_connections {
+		if connections_made >= total_con {
 			break
 		}
-		union(e.i, e.j)
+		
+		connections_made++ // Count every attempt
+		if union(conn.j1, conn.j2) {
+			actual_merges++ // Count only successful merges
+		}
 	}
 	
-	// Count sizes of each group
-	groupSizes := make(map[int]int)
+	fmt.Printf("Attempted connections: %d, Actual merges: %d\n", connections_made, actual_merges)
+	
+	// Count final group sizes
+	finalGroups := make(map[int]int)
 	for i := 0; i < len(junction_data); i++ {
 		root := find(i)
-		groupSizes[root] = groupSize[root]
+		finalGroups[root] = groupSize[root]
 	}
 	
-	// Get the three largest groups
-	sizes := []int{}
-	for _, size := range groupSizes {
+	var sizes []int
+	for _, size := range finalGroups {
 		sizes = append(sizes, size)
 	}
 	sort.Sort(sort.Reverse(sort.IntSlice(sizes)))
 	
-	// Print groups for debugging
 	fmt.Printf("Group sizes: %v\n", sizes)
 	
-	return sizes[0] * sizes[1] * sizes[2]
+	if len(sizes) >= 3 {
+		return sizes[0] * sizes[1] * sizes[2]
+	}
+	result := 1
+	for _, size := range sizes {
+		result *= size
+	}
+	return result
 }
+
 
 //subroutines
 //load data input
@@ -142,6 +152,7 @@ func containsConnection(connections [][]int, j1 int) int {
 	return index
 }
 
+
 //main function
 func main() {
 	fmt.Println("Advent of Code 2025 - Day 8: Junction Box")
@@ -149,7 +160,7 @@ func main() {
 	connections := 10  //default number of connections
 	if len(os.Args) > 1{
 		fileName = os.Args[1]
-		connections = 1000  // Connect 1000 pairs for input.txt
+		connections = 1000 // Connect the 1000 closest pairs
 	}
 	// load junction data
 	junction_data := loadJunctionData(fileName)
